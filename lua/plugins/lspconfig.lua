0260f5b95803836a3002011f3a78478376baf5ff
Peter <peterwalker0695@gmail.com>
2024-08-26 17:03:12 +0530

    lazyvim

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				-- Server-specific settings. See `:help lspconfig-setup`
+				settings = {
+					["rust-analyzer"] = {
+						diagnostics = {
+							enable = false,
+						},
+					},
+				},
+			})
+		end,
+	},
+}
